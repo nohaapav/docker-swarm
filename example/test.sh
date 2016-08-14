73b5dfa30ebfc6e287ebf2b5745451966b97d43a
Pavol Noha <pavol.noha@gmail.com>
2016-08-14 20:31:31 +0200

    test script to check if round-robin and DNS service discovery works

--- example/test.sh
+++ example/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --rm --net=example_back-tier busybox wget -q -O- http://manager:8080
+source ../logger.sh
+
+MANAGER_IP=$(docker-machine ip manager0)
+
+log "Test round-robin DNS:"
+for i in {1..6}; do curl http://$MANAGER_IP; done
+
+log "Test DNS service discovery:"
+for i in {1..6}; do curl http://$MANAGER_IP/redirect/serviceB; done
