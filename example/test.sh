8fa708398567abe98657adb4a2308cc5b6d9329a
Pavol Noha <pavol.noha@gmail.com>
2016-08-23 15:31:14 +0200

    refactoring

--- example/test.sh
+++ example/test.sh
@@ -4,8 +4,8 @@ source ../logger.sh
 
 MANAGER_IP=$(docker-machine ip manager0)
 
-log "Test round-robin LB:"
+log "Test IPVS LB:"
 for i in {1..6}; do curl http://$MANAGER_IP; done
 
-log "Test DNS service discovery:"
-for i in {1..6}; do curl http://$MANAGER_IP/redirect/serviceB; done
+log "Test DNS RR:"
+for i in {1..6}; do curl http://$MANAGER_IP/redirect/service; done
