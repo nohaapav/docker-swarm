06fbec3f89b6e1c95b905e44ca30fb45b6aac55c
Pavol Noha <pavol.noha@gmail.com>
2017-01-22 18:55:46 +0100

    Update test.sh

--- example/test.sh
+++ example/test.sh
@@ -7,5 +7,5 @@ MANAGER_IP=$(docker-machine ip manager0)
 log "Test IPVS LB:"
 for i in {1..6}; do curl http://$MANAGER_IP; done
 
-log "Test DNS RR:"
+log "Test VIP DNS RR:"
 for i in {1..6}; do curl http://$MANAGER_IP/redirect/service; done
